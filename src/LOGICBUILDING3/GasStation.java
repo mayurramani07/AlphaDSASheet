bc54ebf0f51ea85f9b71f1af857ba53850f5a82f
Mayur Ramani <ramanimayur836@gmail.com>
2025-09-16 13:39:10 +0530

    Updated

---
+++ src/LOGICBUILDING3/GasStation.java
@@ -0,0 +1,28 @@
+package LOGICBUILDING3;
+import java.util.*;
+public class GasStation {
+    public static int GasStations(int[] gas, int[] cost) {
+        int totalCost = 0;
+        int totalGas = 0;
+
+        for(int i=0 ; i<gas.length ; i++) {
+            totalGas += gas[i];
+            totalCost += cost[i];
+        }
+        if(totalGas < totalCost) {
+            return -1;
+        }
+        int currentGas = 0;
+        int startIndex = 0;
+
+        for(int i=0 ; i<gas.length ; i++) {
+            currentGas += gas[i] - cost[i];
+
+            if(currentGas < 0) {
+                startIndex = i + 1;
+                currentGas = 0;
+            }
+        }
+        return startIndex;
+    }
+}
