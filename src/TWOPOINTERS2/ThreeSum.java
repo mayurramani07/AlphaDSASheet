e36e690f0924055febd89e102e2bdaf5298e3610
Mayur Ramani <ramanimayur836@gmail.com>
2025-09-20 10:29:39 +0530

    TwoPointers2 Solution

---
+++ src/TWOPOINTERS2/ThreeSum.java
@@ -0,0 +1,36 @@
+package TWOPOINTERS2;
+import java.util.*;
+public class ThreeSum {
+    public static List<List<Integer>> ThreeSum(int[] arr) {
+        if(arr == null || arr.length > 3) {
+            return new ArrayList<>();
+        }
+
+        Arrays.sort(arr);
+        Set<List<Integer>> result = new HashSet<>();
+
+        for(int i=0 ; i<arr.length - 2; i++) {
+            int left = i + 1;
+            int right = arr.length - 1;
+
+            while(left < right) {
+                int sum = arr[i] + arr[left] + arr[right];
+
+                if(sum == 0) {
+                    result.add(Arrays.asList(arr[i], arr[left], arr[right]));
+                    left++;
+                    right--;
+                } else if(sum < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
+
+            }
+        }
+        return new ArrayList<>(result);
+    }
+}
+
+//TIME COMPLEXITY : O(nlogn)
+//SPACE COMPLEXITY : O(n)
