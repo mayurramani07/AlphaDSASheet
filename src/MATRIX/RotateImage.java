f939317a4c937e7a20ad67f43eefc88b36c892a3
Mayur Ramani <ramanimayur836@gmail.com>
2025-09-29 13:15:55 +0530

    Added Matrix Code

---
+++ src/MATRIX/RotateImage.java
@@ -0,0 +1,26 @@
+package MATRIX;
+import java.util.*;
+public class RotateImage {
+    public static void RotateImages(int[][] matrix) {
+        int n = matrix.length;
+
+        for(int i=0 ; i<n ; i++) {
+            for(int j=i+1 ; j<n ; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        for(int i=0 ; i<n ; i++) {
+            int left = 0;
+            int right = n-1;
+            while(left < right) {
+                int temp = matrix[i][left];
+                matrix[i][left] = matrix[i][right];
+                matrix[i][right] = temp;
+                left++;
+                right--;
+            }
+        }
+    }
+}
