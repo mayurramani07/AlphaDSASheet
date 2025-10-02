8f417f57c0f729f94674d58138460153e029b0a4
Mayur Ramani <ramanimayur836@gmail.com>
2025-10-02 23:07:55 +0530

    Update HashMap

---
+++ src/HASHMAP/CountPairWhoseSumLessThanTarget.java
@@ -0,0 +1,16 @@
+package HASHMAP;
+import java.util.*;
+public class CountPairWhoseSumLessThanTarget {
+    public static int CountPairs(int[] arr, int target) {
+        int count = 0;
+
+        for(int i=0 ; i<arr.length ; i++) {
+            for(int j=i+1 ; j<arr.length ; j++) {
+                if(arr[i] + arr[j] < target) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
---
+++ src/HASHMAP/TwoSum.java
@@ -0,0 +1,17 @@
+package HASHMAP;
+import java.util.*;
+public class TwoSum {
+    public static int[] TwoSum1(int[] nums, int target) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+
+        for(int i=0 ; i<nums.length ; i++) {
+            int complement = target - nums[i];
+
+            if(map.containsKey(complement)) {
+                return new int[] {map.get(complement), i};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[] {};
+    }
+}
