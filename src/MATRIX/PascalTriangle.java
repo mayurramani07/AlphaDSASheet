5d8c9dc6162b2628733ad2e667893bacc40a57f5
Mayur Ramani <ramanimayur836@gmail.com>
2025-10-02 20:48:28 +0530

    Added Matrix

---
+++ src/MATRIX/PascalTriangle.java
@@ -0,0 +1,23 @@
+package MATRIX;
+import java.util.*;
+public class PascalTriangle {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        for(int i=0 ; i<numRows ; i++) {
+            List<Integer> row = new ArrayList<>();
+
+            for(int j=0 ; j<=i ; j++) {
+                if(j == 0 || j == i) {
+                    row.add(1);
+                } else {
+                    int aboveLeft = result.get(i-1).get(j-1);
+                    int aboveRight = result.get(i-1).get(j-1);
+                    row.add(aboveLeft + aboveRight);
+                }
+            }
+            result.add(row);
+        }
+        return result;
+    }
+}
