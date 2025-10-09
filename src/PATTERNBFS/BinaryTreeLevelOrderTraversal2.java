57c88a44373349c71570be23c1b7c888eefcbd2c
Mayur Ramani <ramanimayur836@gmail.com>
2025-10-09 15:01:34 +0530

    Learning LC Pattern

---
+++ src/PATTERNBFS/BinaryTreeLevelOrderTraversal2.java
@@ -0,0 +1,47 @@
+package PATTERNBFS;
+import java.util.*;
+public class BinaryTreeLevelOrderTraversal2 {
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+
+        public Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+    public static List<List<Integer>> LevelOrder(Node root) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        if(root == null) {
+            return result;
+        }
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+
+        while(!q.isEmpty()) {
+            int levelSize = q.size();
+            List<Integer> currentLevel = new ArrayList<>();
+
+            for(int i=0 ; i<levelSize ; i++) {
+                Node currNode = q.remove();
+                currentLevel.add(currNode.data);
+
+                if(currNode.left != null) {
+                    q.add(currNode.left);
+                }
+                if (currNode.right != null) {
+                    q.add(currNode.right);
+                }
+            }
+            result.add(currentLevel);
+        }
+        Collections.reverse(result);
+        return result;
+    }
+}
+
+//TIME COMPLEXITY : O(n)
+//SPACE COMPLEXITY : O(n)
