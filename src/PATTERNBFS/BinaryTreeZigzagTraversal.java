dfff13e7387ed37630d81d29f6f484eca93eb70e
Mayur Ramani <ramanimayur836@gmail.com>
2025-10-09 16:50:30 +0530

    Learning LC Pattern

---
+++ src/PATTERNBFS/BinaryTreeZigzagTraversal.java
@@ -0,0 +1,48 @@
+package PATTERNBFS;
+import java.util.*;
+public class BinaryTreeZigzagTraversal {
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+
+        public Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+    public static List<List<Integer>> levelOrder(Node root) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        if(root == null) {
+            return null;
+        }
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        boolean leftToRight = true;
+
+        while(!q.isEmpty()) {
+            int levelSize = q.size();
+            List<Integer> currentLevel = new ArrayList<>();
+
+            for(int i=0 ; i<levelSize ; i++) {
+                Node currNode = q.remove();
+                currentLevel.add(currNode.data);
+
+                if(currNode.left != null) {
+                    q.add(currNode.left);
+                }
+                if(currNode.right != null) {
+                    q.add(currNode.right);
+                }
+            }
+            if(!leftToRight) {
+                Collections.reverse(currentLevel);
+            }
+            result.add(currentLevel);
+            leftToRight = !leftToRight;
+        }
+        return result;
+    }
+}
