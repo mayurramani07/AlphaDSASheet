ef8d20edd2ad7463dfad33c9d4f9cba656f549ef
Mayur Ramani <ramanimayur836@gmail.com>
2025-10-24 14:26:37 +0530

    Solved Q27

---
+++ src/LCTOP150/BestTimeToBuyAndSell3.java
@@ -0,0 +1,18 @@
+package LCTOP150;
+import java.util.*;
+public class BestTimeToBuyAndSell3 {
+    public static int BuyAndSell(int[] prices) {
+        int firstBuy = Integer.MIN_VALUE;
+        int firstSell = 0;
+        int secondBuy = Integer.MIN_VALUE;
+        int secondSell = 0;
+
+        for(int price : prices) {
+            firstBuy = Math.max(firstBuy, -price);
+            firstSell = Math.max(firstSell, firstBuy + price);
+            secondBuy = Math.max(secondBuy, firstSell - price);
+            secondSell = Math.max(secondSell, secondBuy + price);
+        }
+        return secondSell;
+    }
+}
