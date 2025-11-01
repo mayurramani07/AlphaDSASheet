83bf6b1ec1c9ad79a229a3e504e8ecdf0a466bd8
Mayur Ramani <ramanimayur836@gmail.com>
2025-11-01 13:08:05 +0530

    Solved

--- src/LC2TOP150/SmallestMissingIntegerGreatest.java
+++ src/LC2TOP150/SmallestMissingIntegerGreatest.java
@@ -1,4 +1,23 @@
 package LC2TOP150;
-
+import java.util.*;
 public class SmallestMissingIntegerGreatest {
+    public static int missingInteger(int[] nums) {
+        int sum = nums[0];
+        int i = 1;
+
+        while(i <= nums.length && nums[i] == nums[i-1] + 1) {
+            sum += nums[i];
+            i++;
+        }
+        HashSet<Integer> set = new HashSet<>();
+        for(int num : nums) {
+            set.add(num);
+        }
+
+        int x = sum;
+        while(set.contains(x)) {
+            x++;
+        }
+        return x;
+    }
 }
