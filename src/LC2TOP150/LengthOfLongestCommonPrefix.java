ce957f537ba8196c7dd1485243a6f9997dad9866
Mayur Ramani <ramanimayur836@gmail.com>
2025-11-01 13:59:19 +0530

    Soved

--- src/LC2TOP150/LengthOfLongestCommonPrefix.java
+++ src/LC2TOP150/LengthOfLongestCommonPrefix.java
@@ -1,4 +1,24 @@
 package LC2TOP150;
-
+import java.util.*;
 public class LengthOfLongestCommonPrefix {
+    public static int longestCommonPrefix(int[] arr1, int[] arr2) {
+        Set<String> set = new HashSet<>();
+        for(int num : arr1) {
+            String s = String.valueOf(num);
+            for(int i=1 ; i<=s.length() ; i++) {
+                set.add(s.substring(0,i));
+            }
+        }
+        int maxLen = 0;
+        for(int num : arr2) {
+            String s = String.valueOf(num);
+            for (int i = 1; i <= s.length(); i++) {
+                String prefix = s.substring(0, i);
+                if (set.contains(prefix)) {
+                    maxLen = Math.max(maxLen, i);
+                }
+            }
+        }
+        return maxLen;
+    }
 }
