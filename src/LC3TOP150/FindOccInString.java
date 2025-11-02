073e997295fd6c989c0325f9219a12353761d4fc
Mayur Ramani <ramanimayur836@gmail.com>
2025-11-02 12:14:16 +0530

    Solved

---
+++ src/LC3TOP150/FindOccInString.java
@@ -0,0 +1,15 @@
+package LC3TOP150;
+import java.util.*;
+public class FindOccInString {
+    public static int FindOccurence(String haystack, String needle) {
+        int m = haystack.length();
+        int n = needle.length();
+
+        for(int i=0 ; i<m-n ; i++) {
+            if(haystack.substring(i, i+n).equals(needle)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
