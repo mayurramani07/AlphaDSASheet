31deeeea7cf2e87729e7156f24b51fd51ae19db7
Mayur Ramani <ramanimayur836@gmail.com>
2025-11-02 12:22:10 +0530

    Solved

--- src/LC3TOP150/TwoSum.java
+++ src/LC3TOP150/TwoSum.java
@@ -1,5 +1,17 @@
 package LC3TOP150;
 import java.util.*;
 public class TwoSum {
-    public static int TwoSums()
+    public static int[] TwoSums(int[] nums, int target) {
+        HashMap<Integer,Integer> map= new HashMap<>();
+
+        for(int i=0 ; i<nums.length ; i++) {
+            int component = nums[i] - target;
+
+            if(map.containsKey(component)) {
+                return new int[] {map.get(component), i};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[] {};
+    }
 }
