565c1d093f31d395ce850f9c7d7a405d5d9ff788
Mayur Ramani <ramanimayur836@gmail.com>
2025-11-03 00:10:02 +0530

    Solved

--- src/LC3TOP150/ValidPalindrome.java
+++ src/LC3TOP150/ValidPalindrome.java
@@ -1,4 +1,24 @@
 package LC3TOP150;
-
+import java.util.*;
 public class ValidPalindrome {
+    public static boolean isPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+
+        while(left < right) {
+            while(left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+                left++;
+            }
+            while(left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+                right--;
+            }
+
+            if(Character.isLowerCase(left) != Character.isLowerCase(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
 }
