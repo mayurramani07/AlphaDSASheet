9956ccde0e6959497d4d24179b09573af88c89da
Mayur Ramani <ramanimayur836@gmail.com>
2025-11-05 23:52:55 +0530

    Solved

---
+++ src/DPMULTIDIMENSIONAL/Triangle.java
@@ -0,0 +1,18 @@
+package DPMULTIDIMENSIONAL;
+import java.util.*;
+public class Triangle {
+    public static int minimumTotal(List<List<Integer>> Triangle) {
+        int n = Triangle.size();
+
+        int[] dp = new int[n];
+        for(int i=0 ; i<n ; i++) {
+            dp[i] = Triangle.get(n-1).get(i);
+        }
+        for(int row = n-2 ; row >= 0 ; row--) {
+            for(int col = 0 ; col <= row ; col++) {
+                dp[col] = Triangle.get(row).get(col) + Math.min(dp[col], dp[col + 1]);
+            }
+        }
+        return dp[0];
+    }
+}
