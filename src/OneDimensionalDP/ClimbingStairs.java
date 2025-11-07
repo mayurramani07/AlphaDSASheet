ad56938e56ebfb86df11a1386286d02f275bddec
Mayur Ramani <ramanimayur836@gmail.com>
2025-11-07 19:16:23 +0530

    Solved

---
+++ src/OneDimensionalDP/ClimbingStairs.java
@@ -0,0 +1,17 @@
+package OneDimensionalDP;
+import java.util.*;
+public class ClimbingStairs {
+    public static int ClimbingS(int n) {
+        if(n == 1) {
+            return 1;
+        }
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for(int i=3 ; i<=n ; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
+    }
+}
