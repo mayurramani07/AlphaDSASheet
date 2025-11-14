dc262b33113cf94532df46049ccbb639d3e8052e
Mayur Ramani <ramanimayur836@gmail.com>
2025-11-14 12:23:23 +0530

    Solved

---
+++ src/OneDimensionalDP/LongestIncreasingSubs.java
@@ -0,0 +1,24 @@
+package OneDimensionalDP;
+import java.util.*;
+public class LongestIncreasingSubs {
+    public static int LISubsequence(int[] nums) {
+        int n = nums.length;
+        if(n == 0) {
+            return 0;
+        }
+        int[] dp = new int[n];
+        dp[0] = 1;
+        int ans = 1;
+        for(int i=1 ; i<n; i++) {
+            int max = 0;
+            for(int j=0 ; j<i ; j++) {
+                if(nums[i] > nums[j]) {
+                    max = Math.max(max, dp[j]);
+                }
+            }
+            dp[i] = max + 1;
+            ans = Math.max(ans , dp[i]);
+        }
+        return ans;
+    }
+}
