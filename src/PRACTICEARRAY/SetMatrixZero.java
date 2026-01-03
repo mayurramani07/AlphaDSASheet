5a8d4d04ff70378919c2bad30db036d19b748cab
Mayur Ramani <ramanimayur836@gmail.com>
2026-01-04 00:31:06 +0530

    Solved

---
+++ src/PRACTICEARRAY/SetMatrixZero.java
@@ -0,0 +1,39 @@
+package PRACTICEARRAY;
+import java.util.*;
+public class SetMatrixZero {
+    public static void SetMatrix(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        boolean firstColZero = false;
+
+        for(int i=0; i<m ; i++) {
+            if(matrix[i][0] == 0) {
+                firstColZero = true;
+            }
+
+            for(int j=1; j<n ; j++) {
+                if(matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+        for(int i=1; i<m ; i++) {
+            for(int j=1; j<n; j++) {
+                if(matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        if(matrix[0][0] == 0) {
+            for(int j=0; j<n; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+        if (firstColZero) {
+            for (int i = 0; i < m; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+}
