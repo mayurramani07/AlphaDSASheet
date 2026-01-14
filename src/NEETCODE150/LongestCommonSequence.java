6e1dce2025430d99eb0828735a4ff04c9df75cef
Mayur Ramani <ramanimayur836@gmail.com>
2026-01-14 17:24:07 +0530

    SolvingNEETCODE

--- src/NEETCODE150/LongestCommonSequence.java
+++ src/NEETCODE150/LongestCommonSequence.java
@@ -1,4 +1,30 @@
 package NEETCODE150;
 import java.util.*;
 public class LongestCommonSequence {
+    public static int LCS(int[] nums) {
+        if(nums == null || nums.length == 0) {
+            return 0;
+        }
+        HashSet<Integer> set = new HashSet<>();
+        for(int num : nums) {
+            set.add(num);
+        }
+        int maxLength = 0;
+        for(int num : set) {
+            if(!set.contains(num - 1)) {
+                int currNum = num;
+                int length = 1;
+
+                while(set.contains(num - 1)) {
+                    currNum++;
+                    length++;
+                }
+                maxLength = Math.max(maxLength , length);
+            }
+        }
+        return maxLength;
+    }
 }
+
+//TIME COMPLEXITY : O(n)
+//SPACE COMPLEXITY : O(n)
