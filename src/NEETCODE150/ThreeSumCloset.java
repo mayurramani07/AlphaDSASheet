c9d7ec89850cc4313297494b53b68967da492fa4
Mayur Ramani <ramanimayur836@gmail.com>
2026-01-15 17:18:32 +0530

    solvingNEETCODE150

---
+++ src/NEETCODE150/ThreeSumCloset.java
@@ -0,0 +1,35 @@
+package NEETCODE150;
+import java.util.*;
+public class ThreeSumCloset {
+    public static int ThreeSum(int[] nums, int target) {
+        if(nums == null || nums.length < 3) {
+            return 0;
+        }
+        int diff = Integer.MAX_VALUE;
+        int closet = 0;
+
+        Arrays.sort(nums);
+        for(int i=0; i<nums.length; i++) {
+            int left = i+1;
+            int right = nums.length - 1;
+
+            while(left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if(sum == target) {
+                    return target;
+                }
+                if(Math.abs(sum - target) < target) {
+                    diff = Math.abs(sum - target);
+                    closet = sum;
+                }
+                if(sum < target) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+        return closet;
+    }
+}
