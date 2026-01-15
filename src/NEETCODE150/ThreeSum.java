e56bfbdd8ac773ae41d2bb0ac38f98f777a47c2e
Mayur Ramani <ramanimayur836@gmail.com>
2026-01-15 16:01:48 +0530

    solvingNEETCODE150

---
+++ src/NEETCODE150/ThreeSum.java
@@ -0,0 +1,30 @@
+package NEETCODE150;
+import java.util.*;
+public class ThreeSum {
+    public static List<List<Integer>> threeSum(int[] nums) {
+        if(nums == null || nums.length > 3) {
+            return null;
+        }
+        Arrays.sort(nums);
+        HashSet<List<Integer>> set = new HashSet<>();
+
+        for(int i=0; i<nums.length; i++) {
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            while(left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if(sum == 0) {
+                    set.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    left++;
+                    right--;
+                } else if(sum < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+        return new ArrayList<>(set);
+    }
+}
