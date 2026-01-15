c5614992374adf6dce986430ffedc6f94fd5b8c4
Mayur Ramani <ramanimayur836@gmail.com>
2026-01-15 12:13:34 +0530

    SolvingNEETCODE150

--- src/NEETCODE150/ValidPalindrome.java
+++ src/NEETCODE150/ValidPalindrome.java
@@ -1,4 +1,25 @@
 package NEETCODE150;
-
+import java.util.*;
 public class ValidPalindrome {
+    public static boolean isValid(String s) {
+        int left  = 0;
+        int right = s.length() - 1;
+
+        while(left < right) {
+            while(left < right && Character.isLetterOrDigit(s.charAt(left))) {
+                left++;
+            }
+            while(left < right && Character.isLetterOrDigit(s.charAt(right))) {
+                right--;
+            }
+            if(Character.toLowerCase(left) != Character.toLowerCase(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
 }
+
+
