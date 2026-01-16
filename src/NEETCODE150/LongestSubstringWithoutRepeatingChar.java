a6fb7b1e9dbf89e5ca7e94e268878c210163f2f2
Mayur Ramani <ramanimayur836@gmail.com>
2026-01-16 15:55:35 +0530

    solvingNEETCODE150

---
+++ src/NEETCODE150/LongestSubstringWithoutRepeatingChar.java
@@ -0,0 +1,25 @@
+package NEETCODE150;
+import java.util.*;
+public class LongestSubstringWithoutRepeatingChar {
+    public static int LongestSubstring(String str) {
+        HashSet<Character> set = new HashSet<>();
+
+        int left = 0;
+        int right = 0;
+        int maxLength = 0;
+
+        while(right < str.length()) {
+            char ch = str.charAt(right);
+
+            if(!set.contains(ch)) {
+                set.add(ch);
+                maxLength = Math.max(maxLength, right - left + 1);
+                right++;
+            } else {
+                set.remove(str.charAt(ch));
+                left++;
+            }
+        }
+        return maxLength;
+    }
+}
