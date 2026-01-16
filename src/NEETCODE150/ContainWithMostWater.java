c6fd5e45495512d72bc40fe49b69e5e2c6505579
Mayur Ramani <ramanimayur836@gmail.com>
2026-01-16 12:32:21 +0530

    solvingNEETCODE150

---
+++ src/NEETCODE150/ContainWithMostWater.java
@@ -0,0 +1,24 @@
+package NEETCODE150;
+import java.util.*;
+public class ContainWithMostWater {
+    public static int ContainMostWater(int[] heights) {
+        int n = heights.length;
+        int left = 0;
+        int right = heights.length - 1;
+        int maxArea = 0;
+
+        while(left < right) {
+            int h = Math.min(heights[left], heights[right]);
+            int width = right - left;
+            int area = h * width;
+            maxArea = Math.max(maxArea, area);
+
+            if(heights[left] < heights[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return maxArea;
+    }
+}
