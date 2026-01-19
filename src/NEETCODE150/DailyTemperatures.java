fa458b0490704dbbe80a805da70e40b48f12afa3
Mayur Ramani <ramanimayur836@gmail.com>
2026-01-20 02:13:35 +0530

    solvingNEETCODE150

---
+++ src/NEETCODE150/DailyTemperatures.java
@@ -0,0 +1,17 @@
+package NEETCODE150;
+import java.util.*;
+public class DailyTemperatures {
+    public static int[] DailyTemperature(int[] t) {
+        int[] result = new int[t.length];
+        Stack<Integer> stack = new Stack<>();
+
+        for(int i=0; i<t.length; i++) {
+            while(!stack.isEmpty() && t[stack.peek()] < t[i]) {
+                int pop = stack.pop();
+                result[pop] = i - pop;
+            }
+            stack.push(i);
+        }
+        return result;
+    }
+}
