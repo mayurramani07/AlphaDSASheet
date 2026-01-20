308f8507cbe149ea1cd979d4db51f7aac3804508
Mayur Ramani <ramanimayur836@gmail.com>
2026-01-20 12:32:13 +0530

    solvingNEETCODE150

---
+++ src/NEETCODE150/CountCollisionsOnRoad.java
@@ -0,0 +1,23 @@
+package NEETCODE150;
+import java.util.*;
+public class CountCollisionsOnRoad {
+    public static int CountCollisions(String direction) {
+        int left = 0;
+        int right = direction.length() - 1;
+
+        while(left < direction.length() && direction.charAt(left) == 'L') {
+            left++;
+        }
+        while(right >= 0 && direction.charAt(right) == 'R') {
+            right--;
+        }
+
+        int collisions = 0;
+        for(int i = left ; i<=right ; i++) {
+            if(direction.charAt(i) != 'S') {
+                collisions++;
+            }
+        }
+        return collisions;
+    }
+}
