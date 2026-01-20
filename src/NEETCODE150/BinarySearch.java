1b100db0f30ba1a646e9ce27f1c69a5aabaf2389
Mayur Ramani <ramanimayur836@gmail.com>
2026-01-20 14:41:29 +0530

    solvingNEETCODE150

---
+++ src/NEETCODE150/BinarySearch.java
@@ -0,0 +1,22 @@
+package NEETCODE150;
+import java.util.*;
+public class BinarySearch {
+    public static int Binarysearch(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while(left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if(mid == target) {
+                return mid;
+            }
+            else if(mid < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
