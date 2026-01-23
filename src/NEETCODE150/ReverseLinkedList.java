7ff267dac2af3871fa6200ee8a30042c417c049a
Mayur Ramani <ramanimayur836@gmail.com>
2026-01-23 23:49:32 +0530

    solvingNEETCODE150

---
+++ src/NEETCODE150/ReverseLinkedList.java
@@ -0,0 +1,43 @@
+package NEETCODE150;
+import java.util.*;
+public class ReverseLinkedList {
+    static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+
+    public static void print() {
+        if (head == null) {
+            System.out.println("LL IS EMPTY");
+            return;
+        }
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public static Node ReverseLL(Node head) {
+        Node prev = null;
+        Node curr = head;
+        Node next;
+
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+}
