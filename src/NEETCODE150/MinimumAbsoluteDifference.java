59b25e1ce7336c9c9e77904bf6c736bcc26cee0c
Mayur Ramani <ramanimayur836@gmail.com>
2026-01-26 13:31:37 +0530

    solvingNEETCODE150

---
+++ src/NEETCODE150/MinimumAbsoluteDifference.java
@@ -0,0 +1,24 @@
+package NEETCODE150;
+import java.util.*;
+public class MinimumAbsoluteDifference {
+    public static List<List<Integer>> MinAbsoluteDiff(int[] nums) {
+        Arrays.sort(nums);
+
+        List<List<Integer>> result = new ArrayList<>();
+        int minDiff = Integer.MAX_VALUE;
+
+        for(int i=0; i<nums.length; i++) {
+            int diff = nums[i+1] - nums[i];
+            if(diff < minDiff) {
+                minDiff = diff;
+                result.clear();
+            }
+            if (diff == minDiff) {
+
+                result.add(Arrays.asList(nums[i], nums[i + 1]));
+            }
+        }
+
+        return result;
+    }
+}
