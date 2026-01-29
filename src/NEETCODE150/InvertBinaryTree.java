0df3968ca37503fcc978c99e56028429cf763c05
Mayur Ramani <ramanimayur836@gmail.com>
2026-01-29 21:26:26 +0530

    solvingNEETCODE150

--- src/NEETCODE150/InvertBinaryTree.java
+++ src/NEETCODE150/InvertBinaryTree.java
@@ -1,4 +1,45 @@
 package NEETCODE150;
-
+import java.util.*;
 public class InvertBinaryTree {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        public Node(int data) {
+            this.data = data;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    static class BinaryTree{
+        static int idx = -1;
+
+        public static Node BuildTree(int[] nodes) {
+            idx++;
+
+            if(nodes[idx] == -1) {
+                return null;
+            }
+            Node newNode = new Node(nodes[idx]);
+            newNode.left = BuildTree(nodes);
+            newNode.right = BuildTree(nodes);
+
+            return newNode;
+        }
+    }
+    public static Node invertTree(Node root) {
+        if(root == null) {
+            return null;
+        }
+
+        Node temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+
+        invertTree(root.left);
+        invertTree(root.right);
+
+        return root;
+    }
 }
