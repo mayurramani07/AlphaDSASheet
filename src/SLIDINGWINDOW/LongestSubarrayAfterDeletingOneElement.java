f12a1a257fd4cc92cf7f6d20695eb849eecb3873
Mayur Ramani <ramanimayur836@gmail.com>
2026-01-29 13:07:17 +0530

    solvingSlidingWindow

---
+++ src/SLIDINGWINDOW/LongestSubarrayAfterDeletingOneElement.java
@@ -0,0 +1,25 @@
+package SLIDINGWINDOW;
+import java.util.*;
+public class LongestSubarrayAfterDeletingOneElement {
+    public static int LongestSubarray(int[] nums) {
+        int start = 0;
+        int zerocount = 0;
+        int result = 0;
+
+        for (int end = 0; end < nums.length; end++) {
+            if (nums[end] == 0) {
+                zerocount++;
+            }
+
+            while (zerocount > 1) {
+                if (nums[start] == 0) {
+                    zerocount--;
+                }
+                start++;
+            }
+            result = Math.max(result, end - start);
+        }
+
+        return result;
+    }
+}
