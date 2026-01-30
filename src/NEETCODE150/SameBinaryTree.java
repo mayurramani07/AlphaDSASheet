79230bf7ef88e5498d9569a65ed4ffdcda62314d
Mayur Ramani <ramanimayur836@gmail.com>
2026-01-30 19:38:28 +0530

    solvingNEETCODE150

---
+++ src/NEETCODE150/SameBinaryTree.java
@@ -0,0 +1,44 @@
+package NEETCODE150;
+import java.util.*;
+public class SameBinaryTree {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        public Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+    static class BinaryTree{
+        static int idx = -1;
+
+        public static Node BuildTree(int[] nodes) {
+            idx++;
+
+            if(nodes[idx] == -1) {
+                return null;
+            }
+
+            Node newNode = new Node(nodes[idx]);
+            newNode.left = BuildTree(nodes);
+            newNode.right = BuildTree(nodes);
+
+            return newNode;
+        }
+    }
+    public static boolean isSameTree(Node p, Node q) {
+        if(p == null && q == null) {
+            return true;
+        }
+        if(p == null || q == null) {
+            return false;
+        }
+        if(p.data != q.data) {
+            return false;
+        }
+        return isSameTree(p.left, q.left) || isSameTree(p.right, q.right);
+    }
+}
