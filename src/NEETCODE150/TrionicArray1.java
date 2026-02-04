b6d6c9f3adcc6154a28291684da95aba21926127
Mayur Ramani <ramanimayur836@gmail.com>
2026-02-04 13:46:18 +0530

    solvingNEETCODE150

---
+++ src/NEETCODE150/TrionicArray1.java
@@ -0,0 +1,26 @@
+package NEETCODE150;
+import java.util.*;
+public class TrionicArray1 {
+    public static boolean TrionicArray(int[] nums) {
+        int n = nums.length;
+
+        int i = 0;
+        while(i+1 < n && nums[i] < nums[i+1]) {
+            i++;
+        }
+        if(i==0 || i == n-1) {
+            return false;
+        }
+        while(i+1 < n && nums[i] > nums[i+1]) {
+            i++;
+
+            if(i==n-1) {
+                return false;
+            }
+        }
+        while(i+1 < n && nums[i] < nums[i+1]) {
+            i++;
+        }
+        return i == n-1;
+    }
+}
