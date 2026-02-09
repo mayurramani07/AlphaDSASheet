8d84e0bbbda72378d86fd454c812a347bd612624
Mayur Ramani <ramanimayur836@gmail.com>
2026-02-09 14:11:37 +0530

    solvingNEETCODE150

---
+++ src/NEETCODE150/MergeAdjacentEqualElements.java
@@ -0,0 +1,22 @@
+package NEETCODE150;
+import java.util.*;
+public class MergeAdjacentEqualElements {
+    public static List<Long> MergeAdjacent(int[] nums) {
+        Stack<Long> stack = new Stack<>();
+        for(long num : nums){
+            while(!stack.isEmpty() && stack.peek() == num) {
+                long x = stack.pop();
+                num += x;
+            }
+            stack.push(num);
+        }
+        List<Long> list = new ArrayList<>();
+        while(!stack.isEmpty()) {
+            list.add(stack.pop());
+        }
+        Collections.reverse(list);
+        return list;
+    }
+}
+//TIME COMPLEXITY : O(n)
+//SPACE COMPLEXITY : O(n)
