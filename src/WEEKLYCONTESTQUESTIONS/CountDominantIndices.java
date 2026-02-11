429ed322ae6499edb592467ef4b1f903d55cabcb
Mayur Ramani <ramanimayur836@gmail.com>
2026-02-11 23:33:42 +0530

    Weekly Contest Questions

---
+++ src/WEEKLYCONTESTQUESTIONS/CountDominantIndices.java
@@ -0,0 +1,26 @@
+package WEEKLYCONTESTQUESTIONS;
+import java.util.*;
+public class CountDominantIndices {
+    public static int CountDominant(int[] nums) {
+        int n = nums.length;
+        int[] suffix = new int[n+1];
+        suffix[n] = 0;
+        for(int i=n-1; i>=0; i--) {
+            suffix[i] = suffix[i+1] + nums[i];
+        }
+
+        int count = 0;
+        for(int i=0; i<n ; i++) {
+            int sum = suffix[i+1];
+            int num = n - i - 1;
+            if(num == 0) {
+                continue;
+            }
+            int avg = sum / num;
+            if(avg < nums[i]) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
