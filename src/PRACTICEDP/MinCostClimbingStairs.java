c29463294d8c9691d1f4246e655ff5bee43857e3
Mayur Ramani <ramanimayur836@gmail.com>
2026-03-21 17:00:34 +0530

    PracticeDP

---
+++ src/PRACTICEDP/MinCostClimbingStairs.java
@@ -0,0 +1,12 @@
+package PRACTICEDP;
+import java.util.*;
+public class MinCostClimbingStairs {
+    public static int minCostClimbing(int[] cost) {
+        int n = cost.length;
+
+        for(int i=2; i<n ; i++) {
+            cost[i] += Math.min(cost[i-1], cost[i-2]);
+        }
+        return Math.min(cost[n-1], cost[n-2]);
+    }
+}
